479105f6da1d9023601b8d4daf9229c24dc86e39
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2023-03-28 14:24:03 -0600

    Materias
    Creado de view de materias

--- src/components/ListaMaterias.vue
+++ src/components/ListaMaterias.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <h4>
-      Hola
-    </h4>
-  </div>
+  <v-container class="pa-10" style="max-width: 718px">
+    <v-row class="d-flex align-center justify-center">
+      <v-col class="auto">
+        <h4
+          class="text-h3 text-center font-weight-bold"
+          style="color: #384FFE"
+        >
+          MATERIAS INSCRITAS
+        </h4>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex align-center justify-center">
+      <v-col class="auto">
+        <h4
+          class="text-h3 text-center font-weight-bold"
+          style="color: #384FFE"
+        >
+          MATERIAS INSCRITAS
+        </h4>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "ListaMaterias"
+  data() {
+    return {
+      datos: ["Titulo de la materia", "La Congeladora", "Lunes, Miercoles & Viernes", "7:00 - 9:00"]
+    }
+  }
 }
 </script>
 
